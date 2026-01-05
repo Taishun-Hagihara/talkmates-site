e1f3babc9590b9aa91e773383bb8a5190dcd7f97
Taishun <hagitaishun@gmail.com>
2026-01-05 10:30:16 +0900

    brush up

--- src/pages/EventDetail.jsx
+++ src/pages/EventDetail.jsx
@@ -38,7 +38,7 @@ export default function EventDetail() {
 
     return (
         <div className="mx-auto w-full max-w-4xl px-4 pb-12 sm:px-6 lg:px-8">
-            {/* Header */}
+            
             <div className="mt-8">
                 <h1 className="text-3xl font-bold tracking-tight text-slate-900 sm:text-4xl">
                     {title}
@@ -73,7 +73,7 @@ export default function EventDetail() {
                 </p>
             </div>
             
-            {/* CTA */}
+
             {event.apply_url && (
                 <div className="mt-6 flex flex-wrap gap-3">
                     <a
