3f38b1e94cd8cc8ba5a99b4b295f297461d3fbcf
Taishun <hagitaishun@gmail.com>
2026-02-08 19:22:30 +0900

    deleted comment

--- src/contexts/LangContext.jsx
+++ src/contexts/LangContext.jsx
@@ -1,5 +1,3 @@
-//ボチボチの理解って感じだけど螺旋的に学習が進んでいくものだとして一旦他のところを勉強する。。
-//propsとuseContextの理解を上げる必要がある
 
 import { createContext, useContext, useMemo, useState } from "react";
 
@@ -7,11 +5,6 @@ const LangContext = createContext(null);
 
 export function LangProvider({ children }) {
     const [lang, setLang] = useState(() => localStorage.getItem("lang") || "ja");
-    //localStorageに保存されているlangの値について評価している
-    //() => localStorage.getItem()とすることでレンダリング時に読み込むのをなくしている。
-//[lang]は依存配列、useMemoは依存配列を取れる
-    //const obj = { A: B, C: D };と書いた時オブジェクトの中で、キーと値を決めてるobj.AがBでobj.CがDであるそれを応用して以下
-    //setItem("lang",,next)でlang問いうキーにnextという値を保存できる。
 
     const value = useMemo(
         () => ({
@@ -27,11 +20,6 @@ export function LangProvider({ children }) {
 
     return <LangContext.Provider value={value}>{children}</LangContext.Provider>;
 }
-//<何か.Provider value={配りたい値}>
-//  {配りたい範囲（子たち）}
-//</何か.Provider>
-//上記３行のように書いたときvalueの値を中に入ってる {children}：配る対象（この中の子たち全員）
-//Contextに対し.Providerが存在する。
 
 export function useLang() {
     const ctx = useContext(LangContext);
